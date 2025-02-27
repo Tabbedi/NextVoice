747dcf91003b1ae2ff607283944dbac589ca7d89
Tabbedi <tordabbedissen@gmail.com>
2025-02-27 15:48:26 +0100

    Add files via upload

--- JS/TjenesterJS.js
+++ JS/TjenesterJS.js
@@ -7,3 +7,25 @@ document.getElementById("searchForm").addEventListener("submit", function(event)
     const newUrl = baseUrl + "?query=" + encodeURIComponent(query); // Append the query parameter
     window.location.href = newUrl; // Redirect to the URL with the query parameter
   });
+
+
+
+// JavaScript to toggle each dropdown
+const dropdownBtns = document.querySelectorAll('.dropdown-btn');
+
+dropdownBtns.forEach(btn => {
+  btn.addEventListener('click', function () {
+    const dropdownContent = this.nextElementSibling; // The associated content div
+
+    // Toggle the open class to activate the animation
+    dropdownContent.parentElement.classList.toggle('open');
+    
+    // Close all other dropdowns except the one clicked
+    const allDropdowns = document.querySelectorAll('.service-dropdown');
+    allDropdowns.forEach(dropdown => {
+      if (dropdown !== this.parentElement && dropdown.classList.contains('open')) {
+        dropdown.classList.remove('open');
+      }
+    });
+  });
+});
